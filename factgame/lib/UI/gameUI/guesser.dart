585c7ec59f038aecf01890405a928a4dd1985180
Minh Christian Tran <54357939+Mintra99@users.noreply.github.com>
2021-02-23 16:43:17 +0100

    Displays different questions

--- factgame/lib/UI/gameUI/guesser.dart
+++ factgame/lib/UI/gameUI/guesser.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
-
+import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:factgame/Controllers/databasehelper.dart';
 
-class ProposerManager extends StatefulWidget{
+class ProposerManager extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _ProposerPageState();
@@ -20,7 +20,8 @@ class _ProposerPageState extends State<ProposerManager> {
   String showtimer = "10";
   String stringResponse;
   List mapResponse;
-
+  String question;
+  int i = 0;
 
   Map<String, Color> btncolor = {
     "True": Colors.indigoAccent,
@@ -28,31 +29,58 @@ class _ProposerPageState extends State<ProposerManager> {
     "Mostly false": Colors.indigoAccent,
     "False": Colors.indigoAccent,
   };
-   Future fitchData() async {
 
-    var response = await http.get('https://fakenews-app.com/api/game/question/');
-    if(response.statusCode==200){
+  Future fitchData() async {
+    var response =
+        await http.get('https://fakenews-app.com/api/game/question/');
+    if (response.statusCode == 200) {
       setState(() {
-        mapResponse = json.decode(response.body)  ;
+        mapResponse = json.decode(response.body);
+        shuffle();
+        showQuestion();
       });
     }
   }
 
-  answerData(String answer_text, int questionid ) async{
+  answerData(String answer_text, int questionid) async {
     String myUrl = "https://fakenews-app.com/api/game/answer/?format=json";
-    final response = await  http.post(myUrl,
-        body: {
-          "answer_text": "$answer_text",
-          "questionid" : "$questionid",
-        } ) ;
+    final response = await http.post(myUrl, body: {
+      "answer_text": "$answer_text",
+      "questionid": "$questionid",
+    });
     var data = json.decode(response.body);
     return response;
   }
 
+  //List shuffle(List items) {
+  void shuffle(){
+    var random = new Random();
+
+    // Go through all elements.
+    for (var i = mapResponse.length - 1; i > 0; i--) {
+      // Pick a pseudorandom number according to the list length
+      var n = random.nextInt(i + 1);
+
+      var temp = mapResponse[i];
+      mapResponse[i] = mapResponse[n];
+      mapResponse[n] = temp;
+    }
+  }
+
+
+
+  void showQuestion() {
+    if (mapResponse.length > 0){
+      question = mapResponse[0]['question_text'].toString();
+      mapResponse.removeAt(0);
+    }
+  }
+
+
   @override
   void initState() {
-    starttimer();
     fitchData();
+    starttimer();
     super.initState();
   }
 
@@ -76,13 +104,14 @@ class _ProposerPageState extends State<ProposerManager> {
           timer = timer - 1;
         }
         showtimer = timer.toString();
-        percentage = timer.toDouble()/10.0;
+        percentage = timer.toDouble() / 10.0;
       });
     });
   }
 
   void nextquestion() {
-     // increase index by 1
+    // increase index by 1
+    showQuestion();
     canceltimer = false;
     timer = 10;
     starttimer();
@@ -98,7 +127,6 @@ class _ProposerPageState extends State<ProposerManager> {
   }
 
   Widget choiceButton(String k) {
-
     return Padding(
       padding: EdgeInsets.symmetric(
         vertical: 10.0,
@@ -121,7 +149,7 @@ class _ProposerPageState extends State<ProposerManager> {
         minWidth: 200.0,
         height: 45.0,
         shape:
-        RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       ),
     );
   }
@@ -130,24 +158,25 @@ class _ProposerPageState extends State<ProposerManager> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //children: <Widget>[
-          children: [
-            new Container(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      children: <Widget>[
-                        BackButton(),
-                        Spacer(),
-                        Text('Score: 100'), //change this line with actual score when made
-                      ],
-                    ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //children: <Widget>[
+        children: [
+          new Container(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    children: <Widget>[
+                      BackButton(),
+                      Spacer(),
+                      Text('Score: 100'),
+                      //change this line with actual score when made
+                    ],
                   ),
-                  /*Container(
+                ),
+                /*Container(
                     height: 100,
                     child: Title(
                         color: Colors.black,
@@ -157,65 +186,61 @@ class _ProposerPageState extends State<ProposerManager> {
                         )),
                     width: double.infinity,
                   ),*/
-                  Container(
-                    padding: EdgeInsets.all(15.0),
-                    child:  mapResponse==null?Container(): Text(
-                      // follow youtube
-                      mapResponse[0]['question_text'].toString(),
-                      style: TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: "Quando",
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    child: Container(
+                Container(
+                  padding: EdgeInsets.all(15.0),
+                  child: mapResponse == null
+                      ? Container()
+                      : Text(
+                          // follow youtube
+                          '$question',
+                          //mapResponse[0]['question_text'].toString(),
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontFamily: "Quando",
+                          ),
+                        ),
+                ),
+                InkWell(
+                  child: Container(
                     child: Column(
                       children: <Widget>[
-                        choiceButton('True') ,
-                        choiceButton('Mostly true' ),
-                        choiceButton('Mostly false' ),
+                        choiceButton('True'),
+                        choiceButton('Mostly true'),
+                        choiceButton('Mostly false'),
                         choiceButton('False'),
                       ],
                     ),
                   ),
-                  onTap:(){
-
-                  } ,)
-
-                ],
-              ),
+                  onTap: () {},
+                )
+              ],
             ),
-            new Container(
-              child: Column(
-                children: [
-                  Container(
+          ),
+          new Container(
+            child: Column(
+              children: [
+                Container(
                     width: 250,
-                      child: LinearProgressIndicator(
+                    child: LinearProgressIndicator(
                         value: percentage,
                         backgroundColor: Colors.grey,
-                          valueColor: new AlwaysStoppedAnimation<Color>(
-                            Colors.blue,
-                          )
-                      )
-                  ),
-                  Container(
-                    child: Text(
-                      '$timer',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 48,
-                      ),
+                        valueColor: new AlwaysStoppedAnimation<Color>(
+                          Colors.blue,
+                        ))),
+                Container(
+                  child: Text(
+                    '$timer',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 48,
                     ),
-                  )
-                ],
-
-              ),
-            )
-          ],
-        )
-      ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
-
